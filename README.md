7f3af9030b02a8f27d95cb60efd5f823c92627b5
Michael Alexis <20210824@lamolina.edu.pe>
2022-12-03 14:39:16 -0500

    Login
    Frontend login terminado.
    Falta el Backend

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+LA MOLINA - UNALM
 # TF_BBDD
- Prototipo: Sistema de trámite documentario
+ Prototipo: Sistema de trámite documentario.
+ 
+ Intento de realizar el prototipo en python usando el módulo Flask
---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "MySecretKey"
+
+@app.route("/")
+def home():   
+    return render_template("index.html")
+
+@app.route("/login")
+def login():   
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
