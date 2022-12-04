f19b8576fae3b583eae3ddc99e5b9c4887134c53
Michael Alexis <20210824@lamolina.edu.pe>
2022-12-03 20:50:49 -0500

    Intento
    Intento de buscador y de dashboard

--- app/app.py
+++ app/app.py
@@ -11,5 +11,10 @@ def home():
 def login():   
     return render_template("login.html")
 
+@app.route("/user/<idUser>")
+def user(idUser):   
+    a = idUser
+    return render_template("index_user.html", data = a)
+
 if __name__ == "__main__":
     app.run(debug = True)
